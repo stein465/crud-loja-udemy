cef0cef6e6352f6aa7a74697c8a7d3640d5a9bd0
Igor Teixeira <igorxdev@gmail.com>
2021-01-18 12:33:38 -0300

    User CRUD

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -13,16 +13,38 @@ import com.example.demo.repositories.UserRepository;
 public class UserService {
 	
 	@Autowired
-	public UserRepository userRepository;
+	public UserRepository repository;
 	
 	public List<User> findall(){		
-		return userRepository.findAll();		
+		return repository.findAll();		
 	}
 	
 	public User findById(Long id){	
-		Optional<User> obj = userRepository.findById(id);
+		Optional<User> obj = repository.findById(id);
 		
 		return obj.get();
+	}
+	
+	public User insert(User obj) {		
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {		
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 
 }
